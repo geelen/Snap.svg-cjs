d3975b7b1c7acce60629a49a4c8b67ae1b77c758
Glen Maddern <glenmaddern@gmail.com>
2016-01-20 13:22:48 +1100

    yeah nah

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,18 @@
 import gulp from 'gulp'
+import concat from 'gulp-concat'
+import gruntfile from 'snapsvg/Gruntfile'
 
 gulp.task('default', _ => {
-  
+  let gruntConfig
+  const mockGrunt = {
+    file: { readJSON: _ => ({}), read: _ => '' },
+    loadNpmTasks() {},
+    registerTask() {},
+    template: { today() {} },
+    initConfig: config => gruntConfig = config
+  }
+  if (!gruntConfig || !gruntConfig.concat || !gruntConfig.concat.target || !gruntConfig.concat.target.src)
+    throw new Error("Gruntfile had unexpected structure!")
+  gruntfile(mockGrunt)
+  console.log(gruntConfig)
 })
