8080ff3923eccb0512d8e5e9a6e7379df43cdbcd
Glen Maddern <glenmaddern@gmail.com>
2016-01-20 13:51:44 +1100

    looks pretty good to me!

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,42 @@
 import gulp from 'gulp'
 import concat from 'gulp-concat'
-import gruntfile from 'snapsvg/Gruntfile'
+import fs from 'fs'
+import assert from 'assert'
+
+const srcPath = __dirname + '/node_modules/snapsvg/src';
+
+/* Extracted from snapsvg's gruntfile. */
+const expectedSourceFiles = [
+  "mina.js",
+  "svg.js",
+  "element.js",
+  "matrix.js",
+  "attr.js",
+  "class.js",
+  "attradd.js",
+  "paper.js",
+  "path.js",
+  "set.js",
+  "equal.js",
+  "mouse.js",
+  "filter.js",
+  "align.js"
+]
+const excludedSourceFiles = [
+  "amd-banner.js",
+  "amd-footer.js",
+  "copy.js"
+]
 
 gulp.task('default', _ => {
-  let gruntConfig
-  const mockGrunt = {
-    file: { readJSON: _ => ({}), read: _ => '' },
-    loadNpmTasks() {},
-    registerTask() {},
-    template: { today() {} },
-    initConfig: config => gruntConfig = config
-  }
-  if (!gruntConfig || !gruntConfig.concat || !gruntConfig.concat.target || !gruntConfig.concat.target.src)
-    throw new Error("Gruntfile had unexpected structure!")
-  gruntfile(mockGrunt)
-  console.log(gruntConfig)
+  /* Start by confirming the upstream source files haven't changed */
+  const sourceFiles = fs.readdirSync(srcPath);
+  assert.deepEqual(sourceFiles.sort(), [...expectedSourceFiles, ...excludedSourceFiles].sort(), "SnapSVG upstream has different files to what we expect. They must have updated it! This will need to be resolved manually.")
+
+  gulp.src([
+    'cjs-head.js',
+    ...expectedSourceFiles.map(f => `${srcPath}/${f}`),
+    'cjs-foot.js'
+  ]).pipe(concat('snap.svg.js'))
+    .pipe(gulp.dest('dist'))
 })
